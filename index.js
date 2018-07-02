a219d8cfdcd87337b4ce46d5d4082257bdf10b0c
Neal Esford <nesford2457@gmail.com>
2018-07-02 14:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays(){
   return 'Happy holidays!'
 }
 function happyHolidaysTo(name){
-  return 'Happy holidays, ${name}!'
+  return ('Happy holidays, ${name}!')
 }
 function happyHolidayTo(holiday,name){
-  return 'Happy ${holiday}, ${name}!'
+  return ('Happy ${holiday}, ${name}!')
 }
 function holidayCountdown(days,holiday){
-  return 'Its ${days} days until ${holiday}!'
+  return ('Its ${days} days until ${holiday}!')
 }
